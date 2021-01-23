78450c24fd06ccaf5871a321083bf2c61d99e157
Rizvan Dev <rizvan.ahmedov@outlook.com>
2021-01-23 10:15:04 +0400

    focusOnSelect
    добавил свойство focusOnSelect: true,
    в slick-carousel

--- js/review-slider.js
+++ js/review-slider.js
@@ -1,32 +1,33 @@
-	$('.links').slick({
-		arrows: true,
-		dots: false,
-		infinite: true,
-		initialSlide: 0,
-		adaptiveHeight: true,
-		draggable: false,
-		slidesToShow: 5,
-		waitForAnimate: false,
-		centerMode: true,
-		centerPadding: '0px',
-		speed: 500,
-		asNavFor: ".review__position",
-		responsive: [{
+$('.links').slick({
+	arrows: true,
+	dots: false,
+	infinite: true,
+	initialSlide: 0,
+	adaptiveHeight: true,
+	draggable: false,
+	slidesToShow: 5,
+	waitForAnimate: false,
+	centerMode: true,
+	focusOnSelect: true,
+	centerPadding: '0px',
+	speed: 500,
+	asNavFor: ".review__position",
+	responsive: [{
 		breakpoint: 480,
 		settings: {
 			slidesToShow: 3,
 			slidesToScroll: 1
 		}
 	}]
-	})
+})
 
-		$('.review__position').slick({
-			arrows: false,
-			dots: false,
-			speed: 500,
-			infinite: true,
-			waitForAnimate: false,
-			slidesToScroll: 1,
-			draggable: false,
-			asNavFor: ".links"
-	})
+$('.review__position').slick({
+	arrows: false,
+	dots: false,
+	speed: 500,
+	infinite: true,
+	waitForAnimate: false,
+	slidesToScroll: 1,
+	draggable: false,
+	asNavFor: ".links"
+})
